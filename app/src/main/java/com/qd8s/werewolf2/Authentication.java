40b51340d67374a37f54fad7355dddcd6589af81
matthew brown <mpbhome@hotmail.com>
2017-06-15 11:56:54 -0600

    Added an authentication layout. It's not connected to anything yet.

---
+++ app/src/main/java/com/qd8s/werewolf2/Authentication.java
@@ -0,0 +1,20 @@
+package com.qd8s.werewolf2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+
+    // Put authentication code here.
+    public void authenticate(View view) {
+
+    }
+
+}
