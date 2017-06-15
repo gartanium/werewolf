bb3d3beaf75f687487fdc42359f839130c59e3b2
matthew brown <mpbhome@hotmail.com>
2017-06-15 12:09:44 -0600

    Added Authentication again

--- app/src/main/java/com/qd8s/werewolf2/MainActivity.java
+++ app/src/main/java/com/qd8s/werewolf2/MainActivity.java
@@ -5,12 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 
     public void startHostGame(View view) {
@@ -22,5 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, JoinGame.class);
         startActivity(intent);
     }
+
+
 }
 
