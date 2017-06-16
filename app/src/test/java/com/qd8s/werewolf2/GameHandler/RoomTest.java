ca7d404cc83d6a2b0f1e9832cfc20ccb948540b0
matthew brown <mpbhome@hotmail.com>
2017-06-15 23:20:45 -0600

    Added AndroidManifest

---
+++ app/src/test/java/com/qd8s/werewolf2/GameHandler/RoomTest.java
@@ -0,0 +1,117 @@
+package com.qd8s.werewolf2.GameHandler;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Matthew on 6/15/2017.
+ */
+public class RoomTest {
+
+
+
+    @Before
+    public void initialize() {
+
+    }
+
+    @Test
+    public void addPlayer() throws Exception {
+
+        int maxLobbySize = 5;
+        Room testObj = new Room(maxLobbySize);
+
+        for(int i = 0; i < maxLobbySize; i++)
+        {
+            testObj.addPlayer(new Player());
+
+            int expected = i + 1;
+            int actual = testObj.get_Player_Count();
+
+            Assert.assertEquals(expected, actual);
+        }
+    }
+
+    @Test
+    public void removePlayer() throws Exception {
+
+        int maxLobbySize = 5;
+        Room testObj = new Room(maxLobbySize);
+
+        for(int i = 0; i < maxLobbySize; i++) {
+            testObj.addPlayer(new Player());
+        }
+
+        for(int i = maxLobbySize - 1; i > 0; i--) {
+
+            // First get our player that is going to be removed.
+            List<Player> results = testObj.getPlayers();
+            Player toRemove = results.get(i);
+
+            testObj.removePlayer(i);
+
+            results = testObj.getPlayers();
+
+            Assert.assertFalse(results.contains(toRemove));
+        }
+
+    }
+
+    @Test
+    public void removePlayerException() throws Exception {
+        Room testObj = new Room();
+
+        try {
+            testObj.removePlayer(0);
+        }
+        catch (IndexOutOfBoundsException exception)
+        {
+            return;
+        }
+
+        Assert.fail("Expected an IndexOutOfBoundsException!");
+    }
+
+    @Test
+    public void removePlayerExceptionTwo() throws Exception {
+        Room testObj = new Room(5);
+        testObj.addPlayer(new Player());
+
+        try {
+            testObj.removePlayer(1);
+        }
+        catch (IndexOutOfBoundsException exception)
+        {
+            return;
+        }
+
+        Assert.fail("Expected an IndexOutOfBoundsException!");
+    }
+
+    /**
+     * Attempts to add a player in a full lobby.
+     * @throws Exception
+     */
+    @Test
+    public void addPlayerException() throws Exception {
+
+        int maxLobbySize = 1;
+        Room testObj = new Room(maxLobbySize);
+        testObj.addPlayer(new Player());
+
+        try {
+
+            testObj.addPlayer(new Player());
+        }
+        catch (IllegalArgumentException exception)
+        {
+            return;
+        }
+
+        Assert.fail("Expected an Illegal Argument Exception!");
+
+    }
+}
