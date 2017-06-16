10a341f1b896413ea1b3f2688e96a8ef604303b8
matthew brown <mpbhome@hotmail.com>
2017-06-16 00:17:00 -0600

    Added the ability for "Host Game" to generate a "Room" in firebase!

--- app/src/main/java/com/qd8s/werewolf2/HostGame.java
+++ app/src/main/java/com/qd8s/werewolf2/HostGame.java
@@ -5,13 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.Gson;
+import com.qd8s.werewolf2.GameHandler.Room;
+
 public class HostGame extends AppCompatActivity {
 
     /**
      * Publishes the Lobby to firebase.
      */
     protected void publishToFirebase() {
-        // Just freaking add the thing.
+        // Generate a Lobby, and add it to Firebase!
+        Integer maxRoomSize = R.integer.max_room_size;
+
+        Room newRoom = new Room(maxRoomSize);
+        Gson gson = new Gson();
+
+        // Gson converts it into a Json string.
+        String dataToFirebase =  gson.toJson(newRoom);
+
+       // String dataToFirebase = "Cruel world!";
+
+        // Now send the data up to Firebase!!
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("The Only Lobby That Matters");
+        myRef.setValue(dataToFirebase);
 
     }
 
@@ -21,8 +40,14 @@ public class HostGame extends AppCompatActivity {
         setContentView(R.layout.activity_host_game);
     }
 
+    /**
+     * Start the GameLobby activity, and publish to firebase!
+     * @param view
+     */
     public void startGameLobby(View view) {
+
         Intent intent = new Intent(this, GameLobby.class);
+        publishToFirebase();
         startActivity(intent);
     }
 
