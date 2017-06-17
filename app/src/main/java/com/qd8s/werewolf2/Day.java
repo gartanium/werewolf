b44ea3c7fceb20abca9d53f7f61540819662db48
dfinger11 <the11thfinger11@gmail.com>
2017-06-17 17:37:36 -0600

    Added the basic framework for nominate, vote and execute verdict.

--- app/src/main/java/com/qd8s/werewolf2/Day.java
+++ app/src/main/java/com/qd8s/werewolf2/Day.java
@@ -1,8 +1,26 @@
 package com.qd8s.werewolf2;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
  * Created by admin on 6/5/17.
  */
 //
 public class Day {
-}
+    public Map<Integer, User> nominate(){
+        Map<Integer, User> nominees = null;
+        return nominees;
+    }
+
+    public Boolean vote(Map<Integer, User> nominees) {
+        Boolean verdict = false;
+        // Guilty = true
+        // Innocent = false
+        return verdict;
+    }
+
+    public void executeVerdict(User target) {
+        target.setAlive(false);
+    }
+ }
