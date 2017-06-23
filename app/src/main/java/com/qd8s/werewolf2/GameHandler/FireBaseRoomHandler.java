d35dd4e2dbff73a2598faf351f32e63ea25a7509
matthew brown <mpbhome@hotmail.com>
2017-06-23 10:36:58 -0600

    FireBaseHandler now has implementation for allowing a Client to join or Host a Lobby.

--- app/src/main/java/com/qd8s/werewolf2/GameHandler/FireBaseRoomHandler.java
+++ app/src/main/java/com/qd8s/werewolf2/GameHandler/FireBaseRoomHandler.java
@@ -9,22 +9,36 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * Created by Matthew on 6/22/2017.
+ * Description: FireBaseRoomHandler is a class for handeling the Client's local version of a Room,
+ * and updating it whenever there is a change in Firebase. Additionaly, it will at some point
+ * contain logic for updating the Firebase's version any time the local one changes. For now,
+ * it allows a Client to join the Room in firebase upon FireBaseRoomHandler initialization if the
+ * Client isn't the Host. If they are the host, then this Handler will go ahead and create a new
+ * Room in Firebase for us and others to join.Then it will have the Client join it.
  */
 
 public class FireBaseRoomHandler {
 
-    DatabaseReference mRef;
-    Room mRoom;
+    // A reference to the room in Firebase
+    private DatabaseReference mRef;
+
+    // The local version of the room.
+    private Room mRoom;
+
+    // The client associated with this handler.
+    private Client mClient;
+
+    // A boolean to determine if the client should be added to the room.
+    //  If false, when the next update occurs, the client will join.
+    // Then it should be set to false.
+    private boolean mJoinRoom;
 
     // Max players in a room.
     private final int maxPlayers = 12;
+
+    // Tag for debugging.
     private static final String TAG = "FireBaseHandler";
 
     /**
@@ -32,24 +46,20 @@ public class FireBaseRoomHandler {
      * @param roomID ID of the room.
      */
     public FireBaseRoomHandler(Client client, String roomID) {
-
+        mJoinRoom = false;
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         mRef = database.getReference(roomID);
+        mClient = client;
 
-        // Does the room exist?
-            // No?
-                // Create it, set the player as host!
-            // Yes?
-                // Join it.
-
-        // For now, this code will implement the above until I can figure it out.
+        // If client is the host, Implement host logic!
         if(client.is_host()) {
             Log.v(TAG, "Client is hosting the game! " + client.get_ID());
-            addRoom(client, roomID); // Adds a new room to firebase.
+            hostRoom(client, roomID); // Adds a new room to firebase.
+            mJoinRoom = true;
         }
+        // If they aren't the host, allow the room to get the instance from Firebase!
         else {
-            initializeJoinGameRoom(client);  // Sets this room's reference to Firebase's version on update.
-            addClientToRoom(client);
+            addEventListener(mRef);
         }
     }
 
@@ -57,15 +67,20 @@ public class FireBaseRoomHandler {
      * Add a room to FireBase.
      * @param roomID Name of the new Room.
      */
-    private void addRoom(Client client, String roomID) {
+    private void hostRoom(Client client, String roomID) {
 
+        // Create a new local Room.
         mRoom = new Room(maxPlayers, roomID);
         mRoom.addClient(client);
 
+        // Serialize it.
         Gson gson = new Gson();
         String dataToFirebase = gson.toJson(mRoom);
+
+        // Send the data to firebase.
         mRef.setValue(dataToFirebase);
 
+        // Add a listener to the reference, to detect changes.
         addEventListener(mRef);
     }
 
@@ -78,55 +93,49 @@ public class FireBaseRoomHandler {
         ValueEventListener roomListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                //Post post = dataSnapshot.getValue(Post.class);
-                String roomJson = dataSnapshot.getValue(String.class);
 
+                // Get the serialized data from Firebase
+                String roomJson = dataSnapshot.getValue(String.class);
                 Gson gson = new Gson();
                 mRoom = gson.fromJson(roomJson, Room.class);
 
-                Log.v(TAG, "Updating Room! Client Name: " + mRoom.getClients().get(0).get_ID());
+                // If the Client isn't in the room, have them join it!
+                if(!mJoinRoom)
+                {
+                    mJoinRoom = true;
+                    addClientToRoom(mClient);
+                }
+
+                // Log for allowing us to know that the room updated.
+                Log.v(TAG, "Updating Room! Clients: " + mRoom.getClients().toString());
+
                 // ...
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 // Getting Post failed, log a message
-                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+                Log.w(TAG, "loadRoom:onCancelled", databaseError.toException());
                 // ...
             }
         };
 
-        // Attach the event listener!
+        // Attach the event listener to the reference!
         ref.addValueEventListener(roomListener);
 
     }
 
-    private void initializeJoinGameRoom(Client client) {
-        mRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-
-                        //Room room. Get First time use.
-                        String roomJson = dataSnapshot.getValue(String.class);
-
-                        Gson gson = new Gson();
-                        mRoom = gson.fromJson(roomJson, Room.class);
-
-                        Log.v(TAG, "Updating Room! Client Name: " + mRoom.getClients().get(0).get_ID());
-                        // ...
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-
-                    }
-                });
-    }
-
+    // Add the client to the Firebase Room!
     private void addClientToRoom(Client client) {
+
+        // Change our local version first.
         mRoom.addClient(client);
-        mRef.setValue(mRoom);
+
+        // Serialize it, and send it up to Firebase.
+        Gson gson = new Gson();
+        String dataToFirebase = gson.toJson(mRoom);
+        mRef.setValue(dataToFirebase);
+
     }
 
 
