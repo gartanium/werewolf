eec93f6ab8168d4cd50dfbeae9ab7f1e5b22b020
dfinger11 <the11thfinger11@gmail.com>
2017-06-27 11:48:50 -0600

    Added the basic framework for DayMain, DaySecond, NightDoc, NightVillager, NightWolf.

---
+++ app/src/main/java/com/qd8s/werewolf2/NightDoc.java
@@ -0,0 +1,13 @@
+package com.qd8s.werewolf2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NightDoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_night_doc);
+    }
+}
