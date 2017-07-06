9f8f44c3bd7665ab4a52e256ac2e10e0b7d677b6
dfinger11 <the11thfinger11@gmail.com>
2017-07-06 12:47:48 -0600

    Added the basic framework for DayMain, DaySecond, NightDoc, NightVillager, NightWolf.

---
+++ app/src/main/java/com/qd8s/werewolf2/LoseScreen.java
@@ -0,0 +1,15 @@
+package com.qd8s.werewolf2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.qd8s.werewolf2.R;
+
+public class LoseScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lose_screen);
+    }
+}
