4e07c9f83e0615d5cb76c21ed9036ab5048315de
dfinger11 <the11thfinger11@gmail.com>
2017-07-11 13:15:14 -0600

    Added the basic framework for DayMain, DaySecond, NightDoc, NightVillager, NightWolf.

--- app/src/main/java/com/qd8s/werewolf2/Day.java
+++ app/src/main/java/com/qd8s/werewolf2/Day.java
@@ -16,7 +16,7 @@ public class Day {
             Integer votes = 0;
             nominee = userList.get(i);
             for (int j = 0; j < userList.size(); j++) {
-                if (nominee == userList.get(j).getTarget() && userList.get(j).getTarget() != null && nominee.isAlive() == true) {
+                if (nominee == userList.get(j).getTarget() && userList.get(j).getTarget() != null && userList.get(j).isAlive() == true) {
                     votes++;
                     userList.get(j).setTarget(null);
                 }
