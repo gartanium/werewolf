f97beec0fad128649c8433d85048f3504c48177f
dfinger11 <the11thfinger11@gmail.com>
2017-07-11 13:08:55 -0600

    Added the basic framework for DayMain, DaySecond, NightDoc, NightVillager, NightWolf.

--- app/src/main/java/com/qd8s/werewolf2/Day.java
+++ app/src/main/java/com/qd8s/werewolf2/Day.java
@@ -1,5 +1,6 @@
 package com.qd8s.werewolf2;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,46 +9,58 @@ import java.util.Map;
  */
 //
 public class Day {
-    public Integer voteCounter(Integer votes) {
-        return votes + 1;
+    public Map<User, Integer> voteCounter(ArrayList<User> userList) {
+        Map<User, Integer> userVotes = new HashMap<>();
+        User nominee = new User();
+        for (int i = 0; i < userList.size(); i++) {
+            Integer votes = 0;
+            nominee = userList.get(i);
+            for (int j = 0; j < userList.size(); j++) {
+                if (nominee == userList.get(j).getTarget()) {
+                    votes++;
+                }
+            }
+            userVotes.put(nominee, votes);
+        }
+        return userVotes;
     }
 
-    public Map<Integer, User> getTopNominees(Map<Integer, User> nominees) {
-        Map<Integer, User> topNominees = new HashMap<>();
+    public Map<User, Integer> getTopNominees(Map<User, Integer> nominees) {
+        Map<User, Integer> topNominees = new HashMap<>();
         Integer topFirst = 0;
         Integer topSecond = 0;
         User topUser = new User();
         User secondUser = new User();
-        for(Map.Entry<Integer, User> entry : nominees.entrySet()) {
-            if( entry.getKey() >= topFirst) {
+        for (Map.Entry<User, Integer> entry : nominees.entrySet()) {
+            if (entry.getValue() >= topFirst) {
                 topSecond = topFirst;
-                topFirst = entry.getKey();
+                topFirst = entry.getValue();
                 secondUser = topUser;
-                topUser = entry.getValue();
-            } else if(entry.getKey() < topFirst & entry.getKey() > topSecond) {
-                topSecond = entry.getKey();
-                secondUser = entry.getValue();
+                topUser = entry.getKey();
+            } else if (entry.getValue() < topFirst & entry.getValue() > topSecond) {
+                topSecond = entry.getValue();
+                secondUser = entry.getKey();
             }
         }
-        topNominees.put(topFirst, topUser);
-        topNominees.put(topSecond, secondUser);
+        topNominees.put(topUser, topFirst);
+        topNominees.put(secondUser, topSecond);
         return topNominees;
     }
 
-    public User executeVerdict(Map<Integer, User> nominees) {
+    public User executeVerdict(Map<User, Integer> nominees) {
         Integer topVotes = 0;
         Integer secondVotes = 0;
         User guiltyPerson = new User();
-        for(Map.Entry<Integer, User> entry : nominees.entrySet()) {
-            if( entry.getKey() >= topVotes) {
+        for (Map.Entry<User, Integer> entry : nominees.entrySet()) {
+            if (entry.getValue() >= topVotes) {
                 secondVotes = topVotes;
-                topVotes = entry.getKey();
+                topVotes = entry.getValue();
             }
 
             if(topVotes == secondVotes) {
                 guiltyPerson = null;
             } else {
-                guiltyPerson = entry.getValue();
+                guiltyPerson = entry.getKey();
             }
         }
         return guiltyPerson;
